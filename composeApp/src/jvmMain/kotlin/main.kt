5525632e60b9982a174e7fd0e34c76d39246e9ba
brokenhappy <wout.werkman@hotmail.com>
2025-05-22 09:01:56 +0200

    Set default size to match post-challenge view

--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -6,17 +6,15 @@ import androidx.compose.ui.window.rememberWindowState
 import com.woutwerkman.App
 
 fun main(args: Array<String>) = application {
-    val width = args.getOrNull(0)?.toFloatOrNull() ?: 800f
-    val height = args.getOrNull(1)?.toFloatOrNull() ?: 600f
-    val x = args.getOrNull(2)?.toFloatOrNull()
-    val y = args.getOrNull(3)?.toFloatOrNull()
+    val width = args.getOrNull(0)?.toFloatOrNull() ?: 501f
+    val height = args.getOrNull(1)?.toFloatOrNull() ?: 879f
+    val x = args.getOrNull(2)?.toFloatOrNull() ?: 48f
+    val y = args.getOrNull(3)?.toFloatOrNull() ?: 86f
 
     val windowState = rememberWindowState(
         width = width.dp,
         height = height.dp,
-        position = if (x != null && y != null) {
-            WindowPosition(x.dp, y.dp)
-        } else WindowPosition.PlatformDefault,
+        position = WindowPosition(x.dp, y.dp),
     )
 
     Window(
