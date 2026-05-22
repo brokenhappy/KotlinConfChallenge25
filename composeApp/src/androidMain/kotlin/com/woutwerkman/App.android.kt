5f680f024cc58cd18d46bbec33f740b2bdf54179
brokenhappy <wout.werkman@hotmail.com>
2026-05-22 11:10:18 +0200

    Set Android background to white

--- composeApp/src/androidMain/kotlin/com/woutwerkman/App.android.kt
+++ composeApp/src/androidMain/kotlin/com/woutwerkman/App.android.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { App() }
+        setContent {
+            Box(modifier = Modifier.background(Color.White)) {
+                App()
+            }
+        }
     }
 }
