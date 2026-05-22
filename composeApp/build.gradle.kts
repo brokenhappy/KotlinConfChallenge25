2c067e96083b1e51e91ad8166b46014018ff783e
brokenhappy <wout.werkman@hotmail.com>
2026-05-22 15:49:26 +0200

    Add material icons

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -37,6 +37,7 @@ kotlin {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
             implementation(compose.components.resources)
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.datetime)
